8be2702af67a4bae80fa16e9b75416ff57f882dd
Ayodeji Onipe <ayosemail@gmail.com>
2022-01-21 23:46:19 -0500

    Added filter buttons, not functional yet.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import ContentTile from './components/ContentTile';
+import FilterButton from './components/FilterButton';
 import { nanoid } from 'nanoid';
 import TOKEN from './config.js';
 
+/* Note on FILTER_MAP & FILTER_NAME:
+ * Defined outside App() because if not they would be recalculated
+ * each the the <App /> component re-renders. 
+ * This information will never change regardless of what the application does. 
+ */
+const FILTER_MAP = {
+	All: () => true, 
+	Liked: apod => !apod.liked,	/* shows photos whose 'liked' prop is false */
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App() {
 
 	const [apods, setApods] = useState([]);
 	const [isLoadingApods, setisLoadingApods] = useState(true);
+	const [filter, setFilter] = useState('All');
+	const [liked, setLiked] = useState(false);
 
-	var nasaApiKey = process.env.REACT_APP_NASA_API_KEY;
+	const NASA_API_KEY = process.env.REACT_APP_NASA_API_KEY;
 
-	if (!nasaApiKey) {
-		nasaApiKey = TOKEN;
+	if (!NASA_API_KEY) {
+		NASA_API_KEY = TOKEN;
 	}
 	const count = 10;
 
 	useEffect(() => {
-		fetch(`https://api.nasa.gov/planetary/apod?count=${count}&api_key=${nasaApiKey}`)
+		fetch(`https://api.nasa.gov/planetary/apod?count=${count}&api_key=${NASA_API_KEY}`)
 		.then((response) => response.json())
 		.then((data) => setApods([...data]))
 		.then(setisLoadingApods(false))
 	}, []);
 
 	const apodsList = apods
+	.filter(FILTER_MAP[filter])
 	.map(apod => (
 			<ContentTile
 				key={apod.id + nanoid()}
-				id={apod.id}
+				id={apod.id + nanoid()}
 				title={apod.title}
 				src={apod.url}
 				description={apod.explanation}
 				date={apod.date}
+			/>
+		)
+	);
+
+
+	const filterList = FILTER_NAMES.map(name => (
+			<FilterButton 
+				key={name} 
+				name={name} 	
+				isPressed={name === filter}
+				setFilter={setFilter}
 			/>
 		)
 	);
@@ -55,6 +80,9 @@ function App() {
 			<div className='header'>
 				<h1>Spacestagram!</h1>
 				<p>Brought to you by <a href='https://api.nasa.gov'>NASA's image API.</a></p> 
+			</div>
+			<div className='filters btn-group'>
+				{filterList}
 			</div>
 			{isLoadingApods ? loadingContent : loadedContent}
 			<div className='footer'>
--- src/components/ContentTile.js
+++ src/components/ContentTile.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
-// import './ContentTile.css';
-import LikeButton from './LikeButton';
 
 const Image = styled.img`
     border: 2px dashed orange;
@@ -47,19 +45,8 @@ const Date = styled.p`
 
 
     padding: 10px;
+    padding-right: 15px;
     flex: 5;
-`;
-
-const LikeToggle = styled.button`
-    /* Adapt the colors based on primary prop */
-    background: ${props => props.primary ? "palevioletred" : "white"};
-    color: ${props => props.primary ? "white" : "palevioletred"};
-
-    font-size: 1em;
-    font-weight: bold;
-    width: 150px;
-    padding: 10px;
-    border: 2px dashed orange;
 `;
 
 const ButtonAndDate = styled.div`
@@ -85,18 +72,9 @@ const Wrapper = styled.div`
 `;
 
 export default function ContentTile(props) {
-    // var result = format(props.date, 'MM/dd/yyyy')
-    // console.log(result)
-
-    // function linkify(text) {
-    //     var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    //     return text.replace(urlRegex, function(url) {
-    //         return '<a href="' + url + '">' + url + '</a>';
-    //     });
-    // }
-
-    // const description = linkify(props.description);
-
+    
+    const [liked, setLiked] = useState(false);
+    
     return (
         <Wrapper className='content-tile'>
             <Image src={props.src} />
@@ -104,7 +82,9 @@ export default function ContentTile(props) {
             <Description>{props.description}</Description>
     
             <ButtonAndDate>
-                <LikeButton />
+                <div className='likebutton' onClick={() => setLiked(!liked)} >
+                    {liked ? '😍':'🙂' }
+                </div>
                 <Date>{props.date}</Date>
             </ButtonAndDate>
         </Wrapper>
